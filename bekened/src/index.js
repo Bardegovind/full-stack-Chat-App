3de2083f60ea8532403d8b3a4f602f3cc70eb8fe
govind barde <govindbarde703@gmail.com>
2025-07-11 00:26:11 +0530

    fix: serve frontend correctly in production

--- bekened/src/index.js
+++ bekened/src/index.js
@@ -9,7 +9,6 @@ import { app, server } from "./lib/socket.js";
 import path from "path";
 import { fileURLToPath } from "url";
 
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
@@ -35,13 +34,12 @@ app.use("/api/messages", messageRoute);
 
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
-  app.use((req, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../../frontend/dist", "index.html"));
   });
 }
-
 
 server.listen(PORT, () => {
   console.log(`Server running on PORT: ${PORT}`);
