132412248eaffe5bddabc5ba9709837213c5dd82
govind barde <govindbarde703@gmail.com>
2025-07-11 11:06:29 +0530

    Fixed login ui pageh

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -41,14 +41,20 @@ const Login = () => {
   };
 
   return (
+  
     <div className="min-h-screen grid lg:grid-cols-2 bg-base-100">
       
       <div className="flex flex-col justify-center items-center p-6 sm:p-12">
         <div className="w-full max-w-md space-y-8">
-    
+               <br/>
+                <br/>
+                 <br/>
+  
           <div className="text-center mb-8">
             <div className="flex flex-col items-center gap-2 group">
               <div className="w-12 h-12 rounded-xl bg-primary/10 flex items-center justify-center group-hover:bg-primary/20 transition-colors duration-300">
+              
+              
                 <MessageSquare className="w-6 h-6 text-primary group-hover:scale-110 transition-transform" />
               </div>
               <h1 className="text-2xl font-bold mt-2">Welcome Back</h1>
